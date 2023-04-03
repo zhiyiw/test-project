22c5674c85047907e8400d8046c4583713680603
Z.W <privaterbok@gmail.com>
2023-04-03 12:37:36 -0700

    Modify the App.js change name varible

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const name = "XJ"
+  const [count, setCount] = useState(0);
+  const name = "XJ + ZW";
 
   return (
     <div className="App">
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Test v1.0</h1>
+      <h1>Vite + React Test v1.2</h1>
       <h2>{name}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -31,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
